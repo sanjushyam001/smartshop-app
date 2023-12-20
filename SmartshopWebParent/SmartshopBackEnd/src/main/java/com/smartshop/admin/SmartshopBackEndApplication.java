6b2a0700199c3cfff465071699a84fef7ed944db
Sanjeev <kumarsanjuashokit@gmail.com>
2023-12-20 19:17:32 +0530

    Initial Commit

---
+++ SmartshopWebParent/SmartshopBackEnd/src/main/java/com/smartshop/admin/SmartshopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.smartshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartshopBackEndApplication.class, args);
+	}
+
+}
---
+++ SmartshopWebParent/SmartshopFrontEnd/src/main/java/com/smartshop/site/SmartshopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.smartshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartshopFrontEndApplication.class, args);
+	}
+
+}
